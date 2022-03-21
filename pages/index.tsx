0f6e4e8f59004a89e67fb99f84a7ad07b6ab4fc7
Miriuchi98 <98222906+Miriuchi98@users.noreply.github.com>
2022-03-21 15:15:18 +0100

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Sidebar from "../components/Sidebar"
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main>
+        <Sidebar />
+        {/* { Center } */}
+      </main>
+      <div>{/* { Player } */}</div>
+    </div>
+
+  )
+}
+
+export default Home
